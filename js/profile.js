b52c3eebee3c449d1ff53c6768dfc39345357b60
boubaSambare <boubasambare@yahoo.it>
2019-10-28 09:54:39 +0100

    add edit profile

---
+++ js/profile.js
@@ -0,0 +1,8 @@
+function userInfo () {
+    var userName = document.getElementById('inlineFormInputName')
+    userName.value = 'Sambare Aboubacar'
+}
+
+window.onload = function () {
+    userInfo()
+}
